9a202564d5e1970ce9acb315779b91538a818eb5
Prisceva <53707948+Prisceva@users.noreply.github.com>
2020-11-02 14:32:39 +0100

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 const port = process.env.PORT || 3000 ;
 
 app.get("/", function(req, res){
-    res.send("helloWorld !");
+    res.send("helloW!");
 })
 
 app.listen(port, function () {
