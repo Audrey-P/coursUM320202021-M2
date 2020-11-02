639f09021e548c3bb25f65efdeb0168354d51997
Fleur09 <brunfleur@hotmail.fr>
2020-11-02 14:37:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ var app = express();
 
 const port = process.env.PORT || 3000 ;
 
+const fetch = require('node-fetch');
+
+
 app.get("/", function(req, res){
     res.send("helloW!");
+})
+
+app.get("/fetchair/shangai", function(req, res){
+    let url = "http://api.waqi.info/feed/shanghai/?token=demo" ;
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json);
+        res.send("data fetched look your console");
+    });
 })
 
 app.listen(port, function () {
