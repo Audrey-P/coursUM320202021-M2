0bbf1b00a58b15d3207144489df5f32490d395e7
Fleur09 <brunfleur@hotmail.fr>
2020-11-02 15:49:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,28 +21,15 @@ app.use(express.static('docs'));
 
 //ROUTES
 
-
-app.get("/data", function(req, res){
+app.get("/data", cors(corsOptions), function(req, res){
     let url = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-sise-effectifs-d-etudiants-inscrits-esr-public&q=&facet=rentree_lib&facet=etablissement_type2&facet=etablissement_type_lib&facet=etablissement_lib&facet=identifiant_eter&facet=champ_statistique&facet=operateur_lib&facet=localisation_etab&facet=localisation_ins&facet=bac_lib&facet=attrac_intern_lib&facet=dn_de_lib&facet=cursus_lmd_lib&facet=diplome_lib&facet=niveau_lib&facet=disciplines_selection&facet=gd_disciscipline_lib&facet=discipline_lib&facet=sect_disciplinaire_lib&facet=reg_etab_lib&facet=com_ins_lib&facet=uucr_ins_lib&facet=dep_ins_lib&facet=aca_ins_lib&facet=reg_ins_lib" ;
-    https.get(url, (resp) => {
-        let data = '';
-
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-         });
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            console.log("requestair", JSON.parse(data));
-            res.send("data requested look your console");
-        });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-        res.send("nope request didnt work");
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log("fetchair", json);
+        res.send("ça marche ou quooooiii");
     });
 })
-
 
 
 
