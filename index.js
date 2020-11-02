94b06597bde8e7091cb1a7e067aa9817027aee3d
Fleur09 <brunfleur@hotmail.fr>
2020-11-02 15:25:24 +0100

    mise à jour code prof

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ var app = express();
 const port = process.env.PORT || 3000 ;
 
 const fetch = require('node-fetch');
+var https = require('https');
+var cors = require('cors');
 
+var corsOptions = {
+    origin: 'https://Fleur09.github.io',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+
+//serves static files
+app.use(express.static('docs'));
+
+
+//ROUTES
 
 app.get("/", function(req, res){
     let url = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-sise-effectifs-d-etudiants-inscrits-esr-public&q=&facet=rentree_lib&facet=etablissement_type2&facet=etablissement_type_lib&facet=etablissement_lib&facet=identifiant_eter&facet=champ_statistique&facet=operateur_lib&facet=localisation_etab&facet=localisation_ins&facet=bac_lib&facet=attrac_intern_lib&facet=dn_de_lib&facet=cursus_lmd_lib&facet=diplome_lib&facet=niveau_lib&facet=disciplines_selection&facet=gd_disciscipline_lib&facet=discipline_lib&facet=sect_disciplinaire_lib&facet=reg_etab_lib&facet=com_ins_lib&facet=uucr_ins_lib&facet=dep_ins_lib&facet=aca_ins_lib&facet=reg_ins_lib" ;
@@ -17,6 +30,32 @@ app.get("/", function(req, res){
         res.send("fetch okkkkk");
     });
 })
+
+
+
+app.get("/requestair/shangai", function(req, res){
+    let url = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-sise-effectifs-d-etudiants-inscrits-esr-public&q=&facet=rentree_lib&facet=etablissement_type2&facet=etablissement_type_lib&facet=etablissement_lib&facet=identifiant_eter&facet=champ_statistique&facet=operateur_lib&facet=localisation_etab&facet=localisation_ins&facet=bac_lib&facet=attrac_intern_lib&facet=dn_de_lib&facet=cursus_lmd_lib&facet=diplome_lib&facet=niveau_lib&facet=disciplines_selection&facet=gd_disciscipline_lib&facet=discipline_lib&facet=sect_disciplinaire_lib&facet=reg_etab_lib&facet=com_ins_lib&facet=uucr_ins_lib&facet=dep_ins_lib&facet=aca_ins_lib&facet=reg_ins_lib" ;
+    https.get(url, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+         });
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log("requestair", JSON.parse(data));
+            res.send("data requested look your console");
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+        res.send("nope request didnt work");
+    });
+})
+
+
+
 
 app.listen(port, function () {
     console.log('Serveur listening on port ' + port);
