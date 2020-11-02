939c4ae3460d82e0ed1e4d42388a85205ceef4a2
Fleur09 <brunfleur@hotmail.fr>
2020-11-02 14:50:06 +0100

    fetch1_2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const fetch = require('node-fetch');
 
 
 app.get("/", function(req, res){
-    let url = "https://data.enseignementsup-recherche.gouv.fr/explore/dataset/fr-esr-sise-effectifs-d-etudiants-inscrits-esr-public/api/?disjunctive.rentree_lib&fbclid=IwAR00KsUgXQS9VYqlSkteRHighaGmmEf7hSw2mG6bcTs0c_znnUIkn9Ip_5Q" ;
+    let url = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-sise-effectifs-d-etudiants-inscrits-esr-public&q=&facet=rentree_lib&facet=etablissement_type2&facet=etablissement_type_lib&facet=etablissement_lib&facet=identifiant_eter&facet=champ_statistique&facet=operateur_lib&facet=localisation_etab&facet=localisation_ins&facet=bac_lib&facet=attrac_intern_lib&facet=dn_de_lib&facet=cursus_lmd_lib&facet=diplome_lib&facet=niveau_lib&facet=disciplines_selection&facet=gd_disciscipline_lib&facet=discipline_lib&facet=sect_disciplinaire_lib&facet=reg_etab_lib&facet=com_ins_lib&facet=uucr_ins_lib&facet=dep_ins_lib&facet=aca_ins_lib&facet=reg_ins_lib" ;
     fetch(url)
     .then(res => res.json())
     .then(json => {
