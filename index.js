c8a7ac55f4ffd2703545064234419a3e6d644e02
Audrey-P <48600206+Audrey-P@users.noreply.github.com>
2020-11-02 17:32:46 +0100

    Add a new fetch to get only serieL

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ app.use(express.static('docs'));
 
 //ROUTES
 
-app.get("/fetchair/data", cors(corsOptions), function(req, res){
-    let url = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-sise-effectifs-d-etudiants-inscrits-esr-public&q=&facet=rentree_lib&facet=etablissement_type2&facet=etablissement_type_lib&facet=etablissement_lib&facet=identifiant_eter&facet=champ_statistique&facet=operateur_lib&facet=localisation_etab&facet=localisation_ins&facet=bac_lib&facet=attrac_intern_lib&facet=dn_de_lib&facet=cursus_lmd_lib&facet=diplome_lib&facet=niveau_lib&facet=disciplines_selection&facet=gd_disciscipline_lib&facet=discipline_lib&facet=sect_disciplinaire_lib&facet=reg_etab_lib&facet=com_ins_lib&facet=uucr_ins_lib&facet=dep_ins_lib&facet=aca_ins_lib&facet=reg_ins_lib" ;
+//A fetch to get only serieL
+app.get("/data/serieL", cors(corsOptions), function(req, res){
+    let url = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-sise-effectifs-d-etudiants-inscrits-esr-public&q=&facet=rentree_lib&facet=etablissement_type2&facet=etablissement_type_lib&facet=etablissement_lib&facet=identifiant_eter&facet=champ_statistique&facet=operateur_lib&facet=localisation_etab&facet=localisation_ins&facet=bac_lib&facet=attrac_intern_lib&facet=dn_de_lib&facet=cursus_lmd_lib&facet=diplome_lib&facet=niveau_lib&facet=disciplines_selection&facet=gd_disciscipline_lib&facet=discipline_lib&facet=sect_disciplinaire_lib&facet=reg_etab_lib&facet=com_ins_lib&facet=uucr_ins_lib&facet=dep_ins_lib&facet=aca_ins_lib&facet=reg_ins_lib&refine.bac_lib=S%C3%A9rie+L" ;
     fetch(url)
     .then(res => res.json())
     .then(json => {
-        console.log("fetchair", json);
-        res.send("ça marche?");
+        console.log("fetch", json);
+        res.send("Série L");
     });
 })
 
