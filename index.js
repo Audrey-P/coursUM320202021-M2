344e2292b3fd83333b8ff2cd0f046cc234dc79ed
natoine <antoineseilles@gmail.com>
2020-11-03 15:42:08 +0100

    now start server only after fetching data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3001 ;
 
 var fetch = require('node-fetch');
 var https = require('https');
@@ -16,57 +16,71 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
-//serves static files
-app.use(express.static('docs'));
 
+//init some date fetched somewhere
+let initjson = {};
 
-//ROUTES
-
-app.get("/:name", function(req, res){
-    res.send("hello : " + req.params.name );
-})
-
-app.get("/fetchair/shangai", cors(corsOptions), function(req, res){
+async function initialize()
+{
     let url = "http://api.waqi.info/feed/shanghai/?token=demo" ;
-    fetch(url)
-    .then(res => res.json())
-    .then(json => {
-        console.log("fetchair", json);
+    initjson = await fetch(url).then(response => response.json());
+    console.log("initjson", initjson);
+    console.log("now can start server");
 
-        res.format({
-            'text/html': function () {
-            res.send("data fetched look your console");
-            },
-            'application/json': function () {
-                res.setHeader('Content-disposition', 'attachment; filename=score.json'); //do nothing
-                res.set('Content-Type', 'application/json');
-                res.json(json);
-              }
-            })
-    });
-})
+    //serves static files
+    app.use(express.static('docs'));
 
-app.get("/requestair/shangai", function(req, res){
-    let url = "https://api.waqi.info/feed/shanghai/?token=demo" ;
-    https.get(url, (resp) => {
-        let data = '';
+    //ROUTES
 
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-         });
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            console.log("requestair", JSON.parse(data));
-            res.send("data requested look your console");
+    app.get("/:name", function(req, res){
+        res.send("hello : " + req.params.name );
+    })
+
+    app.get("/fetchair/shangai", cors(corsOptions), function(req, res){
+        let url = "http://api.waqi.info/feed/shanghai/?token=demo" ;
+        fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            console.log("fetchair", json);
+
+            res.format({
+                'text/html': function () {
+                res.send("data fetched look your console");
+                },
+                'application/json': function () {
+                    res.setHeader('Content-disposition', 'attachment; filename=score.json'); //do nothing
+                    res.set('Content-Type', 'application/json');
+                    res.json(json);
+                }
+                })
         });
+    })
 
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-        res.send("nope request didnt work");
+    app.get("/requestair/shangai", function(req, res){
+        let url = "https://api.waqi.info/feed/shanghai/?token=demo" ;
+        https.get(url, (resp) => {
+            let data = '';
+
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                console.log("requestair", JSON.parse(data));
+                res.send("data requested look your console");
+            });
+
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            res.send("nope request didnt work");
+        });
+    })
+
+    app.listen(port, function () {
+
+        console.log('Serveur listening on port ' + port);
     });
-})
+}
 
-app.listen(port, function () {
-    console.log('Serveur listening on port ' + port);
-});
+initialize();
