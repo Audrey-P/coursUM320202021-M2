8d786d4456c50cb99a3f6a3732d46a9b0f8f99f6
Fleur09 <brunfleur@hotmail.fr>
2020-11-22 12:04:15 +0100

    Index pour heroku

--- index.js
+++ index.js
@@ -12,85 +12,157 @@ var fs = require("fs"); // Pour lire fichier xml
 var cors = require('cors');
 
 var corsOptions = {
-    origin: 'https://Audrey-P.github.io',
+    origin: 'https://natoine.github.io',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-//init some date fetched somewhere
-let initjson = {};
+  }
 
 async function initialize()
 {
-    let url = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-sise-effectifs-d-etudiants-inscrits-esr-public&q=&rows=100&sort=-rentree&refine.rentree_lib=2018-19" ;
-    initjson = await fetch(url).then(response => response.json());
-    console.log("initjson", initjson);
-    console.log("now can start server");
-
     //serves static files
     app.use(express.static('docs'));
 
     //ROUTES
 
-    // VOCABULAIRE RDF
-    app.get("/rdfvocabulary", cors(corsOptions), function(req, res){
-        //let xml = fs.readFileSync('./docs/RDF.xml');
-        //res.send(xml); // Telecharge le fichier .xml
-    
-        fs.readFile('./docs/RDF.xml', 'utf8', function (err,data) {
-            var xml = data.replace(':domaine:', req.protocol+"://"+req.headers.host); // Pour que cela fonctionne peu importe le protocole
-            res.set('Content-Type', 'application/xml');
-            res.setHeader('Content-disposition', 'attachment; filename=RDF.xml');
-            res.send(xml);
-          });
-    
-    }); 
-
-    // FETCH REGION
-/*
-    app.get("/fetch/:region", cors(corsOptions), function(req, res){
+	    // VOCABULAIRE RDF
+		app.get("/rdfvocabulary", cors(corsOptions), function(req, res){
+			//let xml = fs.readFileSync('./docs/RDF.xml');
+			//res.send(xml); // Telecharge le fichier .xml
+			fs.readFile('./docs/RDF.xml', 'utf8', function (err,data) {
+				var xml = data.replace(':domaine:', req.protocol+"://"+req.headers.host); // Pour que cela fonctionne peu importe le protocole
+				res.set('Content-Type', 'application/xml');
+				res.setHeader('Content-disposition', 'attachment; filename=RDF.xml');
+				res.send(xml);
+			  });
 		
+		}); 
+		
+    app.get("/univs/:region", cors(corsOptions), function(req, res){
 		let data_region = req.params.region;
-        let url = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-sise-effectifs-d-etudiants-inscrits-esr-public&q=&rows=100&sort=-rentree&refine.rentree_lib=2018-19&refine.reg_ins_lib="+ data_region ;
-        fetch(url)
-        .then(res => res.json())
-        .then(json => {
-			let univs = [];
+		let univs = [];
+        let url1 = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-statistiques-sur-les-effectifs-d-etudiants-inscrits-par-etablissement&q=&rows=100&sort=-rentree&refine.rentree_lib=2018-19&refine.reg_etab_lib="+data_region ;
+		let url2 = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-parcoursup&q=&rows=1000&sort=tri&refine.region_etab_aff="+data_region+"&timezone=Europe%2FBerlin";
+		
+        Promise.all([
+			fetch(url1),
+			fetch(url2)
+		]).then(function (responses) {
+			// Get a JSON object from each of the responses
+			return Promise.all(responses.map(function (response) {
+				return response.json();
+			}));
+		}).then(function(json){
+			let records1 = json[0].records;
+			let records2 = json[1].records;
 			let univ = {};
-			json.records.forEach(function(record){
+			
+			records1.forEach(function(record){
 				univ.id = record.fields.etablissement;
-				univ.rentree = record.fields.rentree;
-				univ.academie = record.fields.aca_etab_lib;
 				univ.commune = record.fields.com_etab_lib;
 				univ.region = record.fields.reg_etab_lib;
 				univ.wiki = record.fields.element_wikidata;
 				univ.departement = record.fields.dep_etab_lib;
 				univ.nometablissement = record.fields.etablissement_lib;
 				univ.typeetablissement = record.fields.etablissement_type_lib;
-				univ.type2 = record.fields.etablissement_type2;
-				univ.uucr = record.fields.uucr_etab_lib;
-				univ.eff = record.fields.effectif_total;
+				univ.effectif = record.fields.effectif;
+				univ.capacEtab = 0;
+				univ.effCandidat = 0;
+				univ.effCandPPrincipale = 0;
+				univ.effCandPSecond = 0;
+				univ.effCandAccPorpos = 0;
+				univ.effCandAdmisPrinc = 0;
+				univ.effCandPAdmisSecond = 0;
 				
+				records2.forEach(function(record2){
+					if (univ.id == record2.fields.cod_uai){
+						univ.capacEtab += record2.fields.capa_fin;
+						univ.effCandidat += record2.fields.voe_tot;
+						univ.effCandPPrincipale += record2.fields.nb_voe_pp;
+						univ.effCandPSecond += record2.fields.nb_voe_pc;
+						univ.effCandAccPorpos += record2.fields.acc_tot;
+						univ.effCandAdmisPrinc += record2.fields.acc_pp;
+						univ.effCandPAdmisSecond += record2.fields.acc_pc;
+						
+					}
+					else{
+						return;
+					};
+				});
 				univs.push(univ);
-			})
-			console.log(univs);
-			res.send(univs);
+			});
+			console.log(univs.length);
+			console.log(univs[0]);
+			
+			res.format({/*
+				//let format = req.params.format;
+				'application/xml+rdf': function () {
+					var xmlrdf = '<?xml version="1.0"?>';  
+					xmlrdf = xmlrdf.concat('<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:univvoc="https://cours20202021m2.herokuapp.com/rdfvocabulary">'); //Mettre le lien vers notre vocabulaire 
+					xmlrdf = xmlrdf.concat('<univvoc:Region>');
+					xmlrdf = xmlrdf.concat('<univvoc:hasLibR>').concat(data_region).concat('</univvoc:hasLibR>'); // remplacement data_region par univ.region ??
+					//xmlrdf = xmlrdf.concat('<univvoc:hasCodeR>').concat(univ.coderegion).concat('</univvoc:hasCodeR>'); // Ajout code region
+					//ajouter code region
+					xmlrdf = xmlrdf.concat('<univvoc:hasEtablissement>');
+					univs.forEach(function(univ){
+						xmlrdf=xmlrdf.concat('<univvoc:Etablissement>');
+						xmlrdf = xmlrdf.concat('<univvoc:hasID>').concat(univ.id).concat('</univvoc:hasID>'); 
+						xmlrdf = xmlrdf.concat('<univvoc:hasCom>').concat(univ.commune).concat('</univvoc:hasCom>'); 
+						xmlrdf = xmlrdf.concat('<univvoc:hasName>').concat(univ.nometablissement).concat('</univvoc:hasName>'); 
+						xmlrdf = xmlrdf.concat('<univvoc:hasID>').concat(univ.typeetablissement).concat('</univvoc:hasID>'); 
+						xmlrdf = xmlrdf.concat('<univvoc:hasEff>').concat(univ.effectif).concat('</univvoc:hasEff>'); 
+						xmlrdf=xmlrdf.concat('</univvoc:Etablissement>');
+					})
+					xmlrdf = xmlrdf.concat('</univvoc:hasEtablissement>');
+					xmlrdf = xmlrdf.concat('</univvoc:Region>');
+					xmlrdf = xmlrdf.concat('</rdf:RDF>');
+					//res.setHeader('Content-disposition', 'attachment; filename=trends.xml');
+					res.set('Content-Type', 'application/xml');
+					res.send(xmlrdf);
+					//console.log(xmlrdf);
+				},	*/	
+				'application/json': function () {
+                    res.setHeader('Content-disposition', 'attachment; filename=univs.json'); //do nothing
+                    res.set('Content-Type', 'application/json');
+                    res.json(univs); // Modif json en univs
+				}		
+			});
+        });
+    });
+	
+   app.get("/etab/:etab", cors(corsOptions), function(req, res){
+	let data_etab = req.params.etab;
+	let univs = [];
+        let url = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-statistiques-sur-les-effectifs-d-etudiants-inscrits-par-etablissement&q=&rows=100&sort=-rentree&refine.rentree_lib=2018-19&refine.etablissement="+data_etab ;
+
+	fetch(url)
+	.then(res => res.json())
+	.then(json =>{
+		let univ = {};
+
+		json.records.forEach(function(record){
+			univ.id = record.fields.etablissement;
+			univ.commune = record.fields.com_etab_lib;
+			univ.region = record.fields.reg_etab_lib;
+			univ.wiki = record.fields.element_wikidata;
+			univ.departement = record.fields.dep_etab_lib;
+			univ.nometablissement = record.fields.etablissement_lib;
+			univ.typeetablissement = record.fields.etablissement_type_lib;
+			univ.effectif = record.fields.effectif;
+
+			univs.push(univ);
+		})
+		console.log(univs.length);
+		console.log(univs[0]);
 
             res.format({
-                'text/html': function () {
-                res.send("data fetched look your console");
-                },
                 'application/json': function () {
-                    res.setHeader('Content-disposition', 'attachment; filename=score.json'); //do nothing
+                    res.setHeader('Content-disposition', 'attachment; filename=etab.json'); //do nothing
                     res.set('Content-Type', 'application/json');
-                    res.json(json);
+                    res.json(univs); // Modif json en univs
                 },
-
-			      })
+			})
         });
-    })
-*/
-
-
+    })	
+	
     app.listen(port, function () {
 
         console.log('Serveur listening on port ' + port);
@@ -98,4 +170,3 @@ async function initialize()
 }
 
 initialize();
-
