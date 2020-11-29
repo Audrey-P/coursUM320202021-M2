1541a323eee95bd14c27fd5d541db29232bc4077
emma3466 <emmajottras@hotmail.fr>
2020-11-29 17:29:33 +0100

    nuage de points graphique 2

--- docs/graph2.js
+++ docs/graph2.js
@@ -16,6 +16,8 @@ async function graph2 (){
 
 
 	function draw2(){
+		
+		
 			
 				var svg= d3.select("#svg1");
 				var gContainer= svg.append("g");
@@ -44,27 +46,38 @@ async function graph2 (){
 				var scaleX = d3.scaleLinear();
 				//scaleX.domain([0,300]); 
 				scaleX.domain([0,d3.max(univs, function(d) { return d.capacEtab; })]); //capacité
-				//console.log(d3.max(univs, function(d) { return d.capacEtab; }));
-				scaleX.range([0,500]);
+				console.log(d3.max(univs, function(d) { return d.capacEtab; }));
+				scaleX.range([0,410]);
 					
 				var axisX = d3.axisBottom(scaleX);
 				var gAxisX = gContainer.append("g");
 				gAxisX.call(axisX);
-				gAxisX.attr("transform", "translate(50,375)");		
-
+				gAxisX.attr("transform", "translate(50,375)");	
+				
+		var A = univs.map(function(d) { return d.nometablissement/*.substring(0, 30)*/}); // On récupère les noms des etablissements dans une variable A
+		console.log('Labels etablissements',A);
+		
+		var B = univs.map(function(d) { return d.capacEtab/*.substring(0, 30)*/}); // On récupère les capacités des etablissements
+		console.log('Capacité etablissements',B);
+		
+		var C = univs.map(function(d) { return d.effectif/*.substring(0, 30)*/}); // On récupère les effectifs des etablissements
+		console.log('effectif etablissements',C);
 
                 // Dots
-                //var circle = [];
-                //for(i=0;i<data.length;i++){
-                //    circle[i]=gContainer.append("circle");
-                 //   circle[i].data(data[i]);
-                //    circle[i].attr("class", "point");
-                //    circle[i].attr("cx", scaleX(data[i].natalite) );
-                //    circle[i].attr("cy", scaleY(data[i].mortalite));
-                 //   circle[i].attr("r", z(data[i].population_en_millions));
-                 //   circle[i].attr("transform","translate(20,20)");
-                 //   circle[i].style("fill", c10(data[i].region));
-                //}
+                var circle = [];
+                for(i=0;i<A.length;i++){
+					circle[i]=gContainer.append("circle");
+                    //circle[i].A(A[i]);
+					//console.log(A[5]);
+                    circle[i].attr("class", "point");
+                    circle[i].attr("cx", scaleX(B[i]));
+					//console.log(circle[i].attr("cx", scaleX(B[i])));
+					//console.log(circle[i].attr("cx", scaleX(function(d) { return d.capacEtab; })));
+					circle[i].attr("cy", scaleY(C[i]));
+                    circle[i].attr("r", 1.5);
+                    circle[i].attr("transform","translate(50,27)");
+                    circle[i].style("fill", "dimgrey");
+                }
 
 			
 		// Add X axis label:
