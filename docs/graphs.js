70e14d9ffc087c2d947e4e027252ae7b000e504d
Audrey-P <48600206+Audrey-P@users.noreply.github.com>
2020-12-07 19:38:39 +0100

    Avoid Datatable warning

--- docs/graphs.js
+++ docs/graphs.js
@@ -181,22 +181,6 @@ async function graphs (){
 	$(document).ready(function() {
 		$('#table').DataTable( {
 			paging: false,
-			data: univs,
-			language: {url: "French.json"},
-			columns: [
-				{ title: "Etablissement",data: ".nometablissement" },
-				{ title: "Type",data: ".typeetablissement" },
-				{ title: "Commune",data: ".commune" },
-				{ title: "Capacité d'accueil",data: ".capacEtab" },
-				{ title: "Effectifs inscrits",data: ".effectif" },
-				{ title: "Effectifs admis",data: ".effCandAccPorpos" },
-				{ title: "Informations complémentaires",data: ".wiki" }
-			]
-		} );
-	} );
-
-	$(document).ready(function() {
-		$('#table').DataTable( {
 			destroy: true,
 			searching: false,
 			data: univs,
